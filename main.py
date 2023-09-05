fe52c2c1f69013a157a51c4632b558e43bca6d41
Vivaan Rajesh <63882442+vivaan2006@users.noreply.github.com>
2023-09-05 09:30:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ welder_data = {
     "Propensity_Score_Welder": [0.46, 0.34, 0.57, 0.51, 0.65, 0.57, 0.68, 0.49, 0.57, 0.20, 0.53, 0.50, 0.52, 0.48, 0.57, 0.54, 0.65, 0.19, 0.60, 0.48, 0.60]
 }
 
+
 control_data = {
-    "ID": list(range(1, 28)),
+    "ID": list(range(1, 29)),
     "Age_Control": [45, 47, 39, 41, 34, 31, 35, 41, 34, 50, 44, 42, 40, 44, 35, 38, 36, 52, 36, 42, 38, 30, 38, 40, 38, 42, 38, 40],
     "Race_Control": ['C', 'C', 'C', 'C', 'C', 'AA', 'C', 'AA', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'],
     "Smoker_Control": ['N', 'N', 'Y', 'N', 'N', 'Y', 'N', 'Y', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'Y', 'N', 'N', 'Y', 'N', 'N', 'N', 'Y', 'N', 'Y', 'N', 'Y', 'N'],
     "Propensity_Score_Control": [0.32, 0.28, 0.57, 0.40, 0.67, 0.55, 0.65, 0.35, 0.54, 0.23, 0.47, 0.38, 0.42, 0.34, 0.52, 0.46, 0.64, 0.20, 0.64, 0.38, 0.63, 0.46, 0.64, 0.38, 0.60, 0.38, 0.46, 0.46]
 }
+print(len(control_data["ID"]))
+print(len(control_data["Age_Control"]))
+print(len(control_data["Race_Control"]))
+print(len(control_data["Smoker_Control"]))
+print(len(control_data["Propensity_Score_Control"]))
 
 welder_df = pd.DataFrame(welder_data)
 control_df = pd.DataFrame(control_data)
@@ -32,7 +38,7 @@ matched_controls = []
 
 # matching: find controls within range
 # and select the control with the closest propensity score
-
+# ** optmatch
 for welder_idx in range(len(distance_matrix)):
     potential_controls = np.where(distance_matrix[welder_idx] <= caliper)[0]
     if len(potential_controls) > 0:
@@ -42,3 +48,7 @@ for welder_idx in range(len(distance_matrix)):
 
 for welder_idx, control_idx in matched_pairs:
     print(f"Welder {welder_idx + 1} matched with Control {control_idx + 1}")
+
+print(welder_df)
+print(control_df)
+print(distance_matrix.shape)
